43317728e08609c40f4c894945a2f014f3bb69ea
gucheng <459843397@qq.com>
2024-05-23 20:39:34 +0800

    solve problem success

--- src/main/kotlin/MinWindow76.kt
+++ src/main/kotlin/MinWindow76.kt
@@ -6,78 +6,67 @@ class MinWindow76 {
         }
         val sCharMap = HashMap<Char, Int>()
         var l = 0
-        var r = 0
-        while (l <= r && r < s.length) {
-            if (!tCharMap.contains(s[l])) {
-                l++
-                r++
-            } else {
-                sCharMap[s[l]] = 1
-            }
-
-        }
+        var r: Int
+//        while (r < s.length) {
+//            if (!tCharMap.contains(s[l])) {
+//                l++
+//                r++
+//            } else {
+//                sCharMap[s[l]] = 1
+//            }
+//
+//        }
         var state = 0 //0 init, 1 l is found,to find 2, 3:shrink left 4.
 
-        val stringBuilder = StringBuilder()
         var minLength = Int.MAX_VALUE
         var minString = ""
-        for (i in 0 until s.length) {
-            if (stringBuilder.isEmpty() && !tCharMap.contains(s[i])) {
+        for (i in s.indices) {
+            if (state == 0 && !tCharMap.contains(s[i])) {
                 continue
-            } else if (stringBuilder.isEmpty() && tCharMap.contains(s[i])) {
+            } else if (state == 0 && tCharMap.contains(s[i])) {
                 l = i
-                stringBuilder.append(s[i])
                 sCharMap[s[i]] = 1
                 state = 1 // find first l
-//                if (con)
                 if (containsStringMap(tCharMap, sCharMap, t.length, 1)) {
-                    state = 2
+                    minString = s.substring(i, i + 1)
+                }
+            } else if (state == 1) {
+                sCharMap[s[i]] = (sCharMap[s[i]] ?: 0) + 1
+                if (containsStringMap(tCharMap, sCharMap, t.length, i - l + 1)) {
+                    r = i
+                    state = 2 //found
                     if (i - l + 1 < minLength) {
                         minLength = i - l + 1
-                        minString = stringBuilder.toString()
+                        minString = s.substring(l, r + 1)
                     }
 
                     // shrink left
                     // when contains, update minString and minLength
                     // when not contains, extend r
-
+                    while (l < r) {
+                        sCharMap[s[l]] = (sCharMap[s[l]]?:0) - 1
+                        l++
+                        if (containsStringMap(tCharMap, sCharMap, t.length, r-l+1)) {
+                            if (r-l+1 < minLength) {
+                                minLength = r-l+1
+                                minString = s.substring(l, r + 1)
+                            }
+                        } else {
+                            state = 1
+                            break
+                        }
+                    }
                 }
             } else {
-                if (containsStringMap(tCharMap, sCharMap, t.length, i-l+1)) {
-                    state = 2
-                    if (i - l + 1 < minLength) {
-                        minLength = i - l + 1
-                        minString = stringBuilder.toString()
-                    }
-
-                    // shrink left
-                    // when contains, update minString and minLength
-                    // when not contains, extend r
-
-                }
+                println("state is $state")
             }
-
-            when {
-                stringBuilder.isEmpty() && !tCharMap.contains(s[i]) -> {
-                    continue
-                }
-                stringBuilder.isEmpty() && tCharMap.contains(s[i]) -> {
-                    l = i
-                    r = l
-                    stringBuilder.append(s[i])
-                    sCharMap[s[i]] = 1
-                    state = 1
-                    if (s[i].toString() == t) {
-                        return s[i].toString()
-                    }
-                }
-                state == 1 -> {
-                    
-                }
-
-            }
+//            when {
+//                state == 0 && !tCharMap.contains(s[i]) -> continue
+//                state == 0 && tCharMap.contains(s[i])
+//            }
 
         }
+        println("minString is $minString")
         return minString
     }
 
@@ -101,4 +90,10 @@ class MinWindow76 {
         return true
     }
 }
-// not complete
+
+fun main() {
+    val solution = MinWindow76()
+    solution.minWindow("AD0BEC0DEBANC", "ABC")
+    solution.minWindow("a", "a")
+    solution.minWindow("a", "aa")
+}
